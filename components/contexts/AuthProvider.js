6c5e479cf67a346bc139a7dc9c27fe8b317d832d
huenguyensw <hue.blue@gmail.com>
2023-08-23 17:35:46 +0200

    add asynStorage

--- components/contexts/AuthProvider.js
+++ components/contexts/AuthProvider.js
@@ -1,17 +1,42 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export const AuthContext = createContext();
 
 export default function AuthProvider({children}) {
   const [accessToken, setAccessToken] = useState(null);
 
-  const handleLogin = () =>{
-    setAccessToken('something');
+  const handleLogin = async() =>{
+    try{
+      await AsyncStorage.setItem('accessToken', 'something');
+      setAccessToken('something');
+    }
+    catch(error){
+      console.log(error);
+    }
   }
 
-  const handleLogout = () =>{
-    setAccessToken(null);
+  const handleLogout = async() =>{
+    try{
+      await AsyncStorage.removeItem('accessToken')
+      setAccessToken(null);
+    } catch(error){
+      console.log(error)
+    }
   }
+
+  const isLoggedIn = async() =>{
+    try{
+      const token = await AsyncStorage.getItem('accessToken');
+      setAccessToken(token);
+    }catch(error){
+      console.log(error);
+    }
+  }
+  useEffect(()=>{
+    isLoggedIn();
+  },[])
   return (
     <AuthContext.Provider value={{accessToken,handleLogin,handleLogout}}>
         {children}
